04dbe436f34496a279792b92090fd699229082f3
Joshua Simmons <simmons.44@gmail.com>
2010-07-20 13:35:25 +1000

    Adding OnModeChange hook

--- init.lua
+++ init.lua
@@ -298,6 +298,10 @@ handlers["324"] = function(o, prefix, user, channel, modes)
 	o:invoke("OnChannelModeIs", channel, modes)
 end
 
+handlers["MODE"] = function(o, prefix, target, modes)
+	o:invoke("OnModeChange", parsePrefix(prefix), target, modes)
+end
+
 handlers["ERROR"] = function(o, prefix, message)
 	o:invoke("OnDisconnect", message, true)
 	o:shutdown()
